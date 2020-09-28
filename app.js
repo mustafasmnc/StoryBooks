21efd2d5f9940dd45e91794473dcca215a8145e6
Mustafa <mustafasmnc@gmail.com>
2020-09-28 20:03:31 +0300

    PublicStories

--- app.js
+++ app.js
@@ -19,13 +19,26 @@ connectDB()
 
 const app = express()
 
+// Body parser 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// Handlebars Helper
+const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs({
+  helpers: {
+    formatDate,
+    stripTags,
+    truncate,
+  }, defaultLayout: 'main', extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -50,6 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 3000
 
